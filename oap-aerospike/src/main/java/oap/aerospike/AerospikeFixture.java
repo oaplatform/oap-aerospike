efafe1dac384923a716a6e9432bfd924a96d577b
igor.petrenko <nofateg@users.noreply.github.com>
2023-09-19 15:01:19 +0300

    testcontainers

--- oap-aerospike/src/main/java/oap/aerospike/AerospikeFixture.java
+++ oap-aerospike/src/main/java/oap/aerospike/AerospikeFixture.java
@@ -1,9 +1,15 @@
 package oap.aerospike;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import lombok.Getter;
 import oap.system.Env;
 import oap.testng.AbstractEnvFixture;
 import oap.time.JodaTimeService;
 import oap.util.Dates;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -11,16 +17,17 @@ import java.io.UncheckedIOException;
 /**
  * Env:
  * - AEROSPIKE_TEST_NAMESPACE
- * - AEROSPIKE_HOSTS
- * - AEROSPIKE_PORT
  */
 public class AerospikeFixture extends AbstractEnvFixture<AerospikeFixture> {
     public static final String TEST_NAMESPACE = Env.get( "AEROSPIKE_TEST_NAMESPACE", "test" );
-    public static final String HOST = Env.get( "AEROSPIKE_HOSTS", "localhost" );
-    public static final int PORT = Integer.parseInt( Env.get( "AEROSPIKE_PORT", "3000" ) );
+    public static final String HOST = "localhost";
+    private static final String AEROSPIKE_VERSION = "6.2.0.3_3";
     public final int maxConnsPerNode;
     public final int connPoolsPerNode;
+    @Getter
+    private final int port;
     public AerospikeClient aerospikeClient;
+    private GenericContainer<?> container;
 
     public AerospikeFixture() {
         this( 300, 1 );
@@ -31,33 +38,55 @@ public class AerospikeFixture extends AbstractEnvFixture<AerospikeFixture> {
         this.connPoolsPerNode = connPoolsPerNode;
 
         define( "AEROSPIKE_TEST_NAMESPACE", TEST_NAMESPACE );
-        define( "AEROSPIKE_HOSTS", HOST );
-        define( "AEROSPIKE_PORT", PORT );
+        define( "AEROSPIKE_HOSTS", "localhost" );
+        definePort( "AEROSPIKE_PORT" );
+        port = portFor( "AEROSPIKE_PORT" );
     }
 
     @Override
     protected void before() {
         super.before();
 
-        aerospikeClient = new AerospikeClient( HOST, PORT, true, JodaTimeService.INSTANCE );
+        var portBinding = new PortBinding(
+            Ports.Binding.bindPort( port ),
+            new ExposedPort( 3000 ) );
+
+        container = new GenericContainer<>( DockerImageName.parse( "aerospike/aerospike-server-enterprise:" + AEROSPIKE_VERSION ) )
+            .withCreateContainerCmdModifier( cmd -> cmd.getHostConfig().withPortBindings( portBinding ) );
+        container.start();
+    }
+
+    public AerospikeClient getAerospikeClient( Integer ttl ) {
+        var aerospikeClient = new AerospikeClient( container.getHost(), container.getMappedPort( 3000 ), true, JodaTimeService.INSTANCE );
         aerospikeClient.maxConnsPerNode = maxConnsPerNode;
         aerospikeClient.connPoolsPerNode = connPoolsPerNode;
         aerospikeClient.connectionTimeout = Dates.s( 120 );
         aerospikeClient.start();
         aerospikeClient.waitConnectionEstablished();
-        asDeleteAll();
+        return aerospikeClient;
     }
 
     @Override
-    protected void after() {
-        try {
+    public void beforeMethod() {
+        super.beforeMethod();
+        if( container.isRunning() ) {
+            aerospikeClient = getAerospikeClient( null );
             asDeleteAll();
+        } else {
+            throw new RuntimeException( "Aerospike container was shutdown while being used. Check the scope of the fixture." );
+        }
+    }
+
+    @Override
+    public void after() {
+        try {
             aerospikeClient.close();
+            if( container != null ) {
+                container.stop();
+            }
         } catch( IOException e ) {
             throw new UncheckedIOException( e );
         }
-
-        super.after();
     }
 
     public void asDeleteAll() {
